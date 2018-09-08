c96eb51e56d503ba16d13d15db2d365bb7ff068c
piumi96 <piumidp96@gmail.com>
2018-09-08 12:07:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded( { extended: true }));
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Server running');
+});
+
+app.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    if(username==='Rangana' && password==='rangana123'){
+        res.send({
+            msg: 'true'
+        });
+    }
+    else{
+        res.send({
+            msg:  'false'
+        });
+    }
+});
