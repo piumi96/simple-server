fe6e81c51eeb3b896609b205f35bb2468b27d5d6
Buddhika Chathuranga <vmbckscc456@gmail.com>
2018-09-08 12:24:19 +0530

    cors added

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded( { extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors())
 const port = 3000;
 
 app.listen(port, () => {
